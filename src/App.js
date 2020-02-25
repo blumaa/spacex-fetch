6e9212dd15261840f11f40243fee560878d75bb6
Aaron Blum <blumaa@gmail.com>
2020-02-25 16:28:33 +0100

    lots of sorting through objects and reducing

--- src/App.js
+++ src/App.js
@@ -5,26 +5,83 @@ function App() {
   const [data, setData] = useState();
   // const [query, setQuery] = useState('react');
 
-
   useEffect(() => {
     async function fetchData() {
       // You can await here
-      const res = await fetch(
-        "https://api.spacexdata.com/v3/launches/past"
-      );
+      const res = await fetch("https://api.spacexdata.com/v3/launches/past");
       const json = await res.json();
       console.log(json);
-      setData(json);
+
+      const filteredByYear = json.filter(
+        launch => launch.launch_year === "2018"
+      )
+
+      console.log("look here", filteredByYear);
+
+      const filterTree = (launches, find) => {
+        return launches.reduce(function(acc, launch) {
+          const payloads = launch.rocket.second_stage.payloads.reduce(function(
+            acc,
+            cat
+          ) {
+            const customers = cat.customers.filter(ind => ind.includes(find));
+            return !customers.length
+              ? acc
+              : acc.concat(Object.assign({}, cat, { customers }));
+          },
+          []);
+          return !payloads.length
+            ? acc
+            : acc.concat(Object.assign({}, launch, { payloads }));
+        }, []);
+      }
+
+      const filteredByCustomer = filterTree(filteredByYear, 'NASA')
+
+      console.log('filter by customer', filteredByCustomer)
+
+      // sort them by inverse chronology
+
+
+      const sortedByUtc = filteredByCustomer.sort((a,b)=>{
+        return new Date(b.launch_date_utc) - new Date(a.launch_date_utc);
+      })
+
+      console.log('sorted by utc', sortedByUtc)
+
+      // sort them by payload
+
+      const sortedByPayload = sortedByUtc.sort(function(a, b){
+        return b.rocket.second_stage.payloads.length - a.rocket.second_stage.payloads.length
+      })
+
+      console.log('sortedbypayload', sortedByPayload)
+
+      const finalObj = (({ a, c }) => ({ a, c }))(object);
+      // const filteredByCustomer = filteredByYear.filter((item) => item.rocket.second_stage.payloads.filter((load) => load.customer.includes("NASA"))
+
+
+      setData(sortedByPayload);
     }
     fetchData();
-  }, []); // Or [] if effect doesn't need props or state
+  }, []);
 
-  const filteredLaunches = this.state.data.filter((item) => {item.launch_year === '2018'})
+  console.log(data);
 
-  // const filteredForYear =()=>{ return data.filter((launch) => {launch.launch_year === '2018'}) }
-  // console.log(filteredForYear)
-
-  return <div className="App">space x test</div>;
+  return (
+    <div className="App">
+      <pre>{data ? JSON.stringify(data, null, 2) : "loading ... "}</pre>
+      {/* data
+        ? data.map(item => (
+            <li key={item.flight_number}>
+              flight number:{item.flight_number} | mission name:{" "}
+              {item.mission_name} | payload count: {item.rocket.second_stage.payloads.length} | {item.launch_date_utc}
+            </li>
+          ))
+        : "loading..." */}
+      hi
+    </div>
+  );
 }
 
 export default App;
